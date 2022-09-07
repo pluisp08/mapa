b209e8fbe129b462f9a7309468a866d614c3dc24
pluisp08 <pluisp08@gmail.com>
2022-09-06 23:35:43 -0300

    migration productos - usuarios agregada

--- database/migrations/2022_09_07_014031_create_usuarios_table.php
+++ database/migrations/2022_09_07_014031_create_usuarios_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
+            $table->string('passwword');
+            $table->string('nivel');
             $table->timestamps();
         });
     }
